776b10c62c5d0c9e91b2e35cd648f035f993a76a
jlp-io <jap00031@students.stir.ac.uk>
2018-03-05 15:09:39 -0600

    Update TextField.js
    everything works besides float

--- frontend/src/components/TextField.js
+++ frontend/src/components/TextField.js
@@ -1,38 +1,85 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react'	
 import PropTypes from 'prop-types'
 import Bootstrap from 'react-bootstrap'
 
 class TextField extends Component {
-  /*
-	//propTypes returns syntax error for me but it might work on other machine
-	static PropTypes = {
-        text = PropTypes.string,
-        hint = PropTypes.string,
-        error_message = PropTypes.string,
-        input_type = PropTypes.string
-    }
-	*/
 	constructor(props) {
 		super(props);
-		this.state = {valid: true, id: this.props.text, error_message: ""};
+		this.state = {valid: true, id: this.props.text, error_message: "", type: this.props.typeVal};
 	}
 
-  handleChange(event) {
-	  var input = document.getElementById(this.state.id).innerHTML;
-	  if (!isNaN(input)) {
-	  this.setState({error_message: "error in input"});
-	  } 
-  }
+  handleChange(e) {
+	  var input = e.target.value.split('');
+	  var foundError = false;
+	  
+	  if (this.props.typeVal == "String" || this.props.typeVal == "string") {
+		for (var i = 0; i < input.length; i++) {
+		  if (!isNaN(input[i]) && input[i] != " ") {
+			  foundError = true;
+		  }else{
+			  this.setState({error_message: ""});
+		  }
+		}
+		if (foundError) {
+		  this.setState({error_message: "error in input: should be a word"});
+		}
+	  }
+	  
+	  if (this.props.typeVal == "Int" || this.props.typeVal == "int") {
+		for (var i = 0; i < input.length; i++) {
+			if (isNaN(input[i]) && input[i] != " ") {
+				foundError = true;
+			}else{
+				this.setState({error_message: ""});
+			}
+		}
+		if (foundError) {
+		  this.setState({error_message: "error in input: should be a number"});
+		}
+	  }	  
+	  
+	  if (this.props.typeVal == "Float" || this.props.typeVal == "float") {
+		for (var i = 0; i < input.length; i++) {
+			if (isNaN(input[i]) && input[i] != " ") {
+				 foundError = true;
+			}else{
+				this.setState({error_message: ""});
+			}
+		}
+		
+		/*
+		if (foundError = false) {
+			var inputString;
+			for (var i = 0; i < input.length; i++) {
+					inputString += input[i];
+			}
+			var remainder = inputString & 1;
+			if (remainder > 1) {
+				foundError = true;
+			}
+		}
+		*/
+		
+		if (foundError) {
+			this.setState({error_message: "error in input: should be a number"});
+		}
+	  }
+	 
+	  if (input == "") {
+		  this.setState({error_message: ""});
+	  }
+   }
   
   render() {
 	if (!this.state.valid) {
-		var error = this.props.error_message;
+		var error = this.state.error_message;
 	}
- 	return (
-      		<div id = "className">
-			<input class="form-control input-sm" type={this.props.input_type} id={this.props.text} placeholder={this.props.hint} onChange={this.handleChange.bind(this)} required autofocus /> <br/>
-			<p>{this.state.error_message}</p>
-		</div>
+    return (
+      	<div id = "className">
+		<input class="form-control input-sm" type={this.props.input_type} id={this.props.text}
+                    placeholder={this.props.hint} onChange={(event) => this.handleChange(event)} required autofocus /> <br/>
+		<p>{this.state.error_message}</p>
+	</div>
 	)
   }
 }
