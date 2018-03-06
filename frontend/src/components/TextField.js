642a6319a8b0ad7b37f2579d70be998dd4cfbb25
jlp-io <jap00031@students.stir.ac.uk>
2018-03-05 20:08:20 -0600

    Update TextField.js
    added limits

--- frontend/src/components/TextField.js
+++ frontend/src/components/TextField.js
@@ -9,57 +9,70 @@ class TextField extends Component {
 	}
 
   handleChange(e) {
-	  var value = e.target.value;
-	  var input = value.split('');
-	  var foundError = false;
+	var value = e.target.value;
+	var input = value.split('');
+	var foundError = false;
 	  
 	  if (this.props.typeVal == "String" || this.props.typeVal == "string") {
-		for (var i = 0; i < input.length; i++) {
-		  if (!isNaN(input[i]) && input[i] != " ") {
-			  foundError = true;
-		  }else{
-			  this.setState({error_message: ""});
-		  }
-		}
-		if (foundError) {
-		  this.setState({error_message: "error in input: should be a word"});
+		if (value.length <= this.props.limit) {
+			for (var i = 0; i < input.length; i++) {
+			  if (!isNaN(input[i]) && input[i] != " ") {
+				  foundError = true;
+			  }else{
+				  this.setState({error_message: ""});
+			  }
+			}
+			if (foundError) {
+				this.setState({error_message: "error in input: should be a word"});
+			}
+		}else{
+			foundError = true;
 		}
 	  }
 	  
 	  if (this.props.typeVal == "Int" || this.props.typeVal == "int") {
-		for (var i = 0; i < input.length; i++) {
-			if (isNaN(input[i])) {
-				foundError = true;
-			}else{
-				this.setState({error_message: ""});
+		if (value <= this.props.limit) {
+			for (var i = 0; i < input.length; i++) {
+				if (isNaN(input[i])) {
+					foundError = true;
+				}else{
+					this.setState({error_message: ""});
+				}
 			}
-		}
-		if (foundError) {
-		  this.setState({error_message: "error in input: should be a number"});
+			if (foundError) {
+			  this.setState({error_message: "error in input: should be a number"});
+			}
+		}else{
+			foundError = true;
 		}
 	  }	  
 	  
 	  if (this.props.typeVal == "Float" || this.props.typeVal == "float") {
-		for (var i = 0; i < input.length; i++) {
-			if (isNaN(input[i]) && input[i] != ".") {
-				 foundError = true;
-			}else{
-				this.setState({error_message: ""});
+		if (value <= this.props.limit) {
+			for (var i = 0; i < input.length; i++) {
+				if (isNaN(input[i]) && input[i] != ".") {
+					 foundError = true;
+				}else{
+					this.setState({error_message: ""});
+				}
 			}
-		}
-		
-		if (value.includes(".") === false) {
+			
+			if (value.includes(".") === false) {
+				foundError = true;
+			}
+			
+			if (foundError) {
+				this.setState({error_message: "error in input: should be a decimal number"});
+			}
+		}else{
 			foundError = true;
-		}
-		
-		if (foundError) {
-			this.setState({error_message: "error in input: should be a decimal number"});
 		}
 	  }
 	  
 	if (input == "") {
 		this.setState({error_message: ""});
 	}
+	
    }
   
   render() {
@@ -67,11 +80,11 @@ class TextField extends Component {
 		var error = this.state.error_message;
 	}
     return (
-      	<div id = "className">
-		<input class="form-control input-sm" type={this.props.input_type} id={this.props.text}
+      <div id = "className">
+	<input class="form-control input-sm" type={this.props.input_type} id={this.props.text}
                     placeholder={this.props.hint} onChange={(event) => this.handleChange(event)} required autofocus /> <br/>
-		<p>{this.state.error_message}</p>
-	</div>
+	<p>{this.state.error_message}</p>
+      </div>
 	)
   }
 }
