b266384dec1ae8d4160c85d8b5399382f3df65ea
alan-fang <fang.alan.7@gmail.com>
2018-03-08 23:45:51 -0600

    added compatibilitiye for TextField

--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -9,6 +9,7 @@ import Dropdown from './Dropdown.js'
 import Button from './Button.js'
 import LargeTextInput from './LargeTextInput.js'
 import TextField from './TextField.js'
+import FormOne from './FormOne.js'
 import { BrowserRouter } from 'react-router-dom'
 
 export {
@@ -19,5 +20,6 @@ export {
   Button,
   LargeTextInput,
   Dropdown,
-  TextField
+  TextField,
+  FormOne
 }
