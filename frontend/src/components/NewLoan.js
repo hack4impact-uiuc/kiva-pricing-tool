dcb44051c8acc3387125483eb737ec41884b2ff4
jlp-io <jap00031@students.stir.ac.uk>
2018-03-09 18:04:21 -0600

    Update NewLoan.js

--- frontend/src/components/NewLoan.js
+++ frontend/src/components/NewLoan.js
@@ -1,52 +1,46 @@
 import React, { Component } from 'react'
-import Bootstrap from 'react-bootstrap'
+import 'react-bootstrap-typeahead/css/Typeahead.css'
+import Bootstrap, { Button } from 'react-bootstrap'
 import TextField from './TextField'
+import axios from 'axios'
+import 'bootstrap-select'
+import 'jquery'
+import {Typeahead} from 'react-bootstrap-typeahead'
+
 
 class NewLoan extends Component {
 	constructor(props) {
 		super(props);
-		this.state = {mfiBool: false, loanBool: false, productBool: false, errorMessage: ""};
+		this.state = {error_message: "", multiple: false};
+		var Typeahead = require('react-bootstrap-typeahead').Typeahead;
 	}
 	
-	changeMFI(e) {
-		this.state.mfiBool = true;
-	}
-	
-	changeLoan(e) {
-		this.state.loanBool = true;
-	}
-	
-	changeProduct(e) {
-		this.state.productBool = true;
+	componentDidMount() {
+		axios.get('https://api.github.com/users/jlp-io')
+		  .then(function (response) {
+			  console.log(response.data);
+		  });
+		axios.get('./mfi.json')
+		  .then(function (response) {
+			  console.log(response);
+		  });
+		axios.get('./loan.json')
+		  .then(function (response) {
+			  console.log(response);
+		  });
 	}
 	
 	render() {
-
 		return (
-		// onClick={ (event) => {this.changeMFI(event);}} 
 		<div>
-			<select class="mfiPicker" data-live-search="true">
-			<option data-tokens="ketchup mustard">Select MFI Type</option>
-			<option data-tokens="ketchup mustard">MFI 1</option>
-			<option data-tokens="ketchup mustard">MFI 2</option>
-			<option data-tokens="ketchup mustard">MFI 3</option>
-			</select>
-			<br>
-			</br>
-			<br>
-			</br>
-			<select class="loanPicker" data-live-search="true">
-			<option data-tokens="ketchup mustard">Select Loan Type</option>
-			<option data-tokens="ketchup mustard">Loan 1</option>
-			<option data-tokens="ketchup mustard">Loan 2</option>
-			<option data-tokens="ketchup mustard">Loan 3</option>
-			</select>
-			<br>
-			</br>
-			<br>
-			</br>
-			
-			<TextField typeVal="String" limit="100"  />
+		<Typeahead
+			labelKey={this.props.label}
+			multiple=""
+			options={this.props.list}
+			placeholder ={this.props.hint}
+		/>	
+		<br>
+		</br>
 		</div>
 		)
 	}
