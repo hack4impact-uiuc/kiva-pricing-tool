aa5cbb57e8ae4702a600cdd809e994532b5a0cc4
jlp-io <jap00031@students.stir.ac.uk>
2018-03-09 18:00:55 -0600

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react'
-import { Grid, Navbar, Jumbotron } from 'react-bootstrap'
+import { Forms, Dropdowns, Grid, Navbar, Jumbotron } from 'react-bootstrap'
+import Bootstrap from 'react-bootstrap'
 import './styles/app.css'
 import TextField from './Components/TextField'
 import NewLoan from './Components/NewLoan'
 
 class App extends Component {
   render() {
-    return (
+	  var mfiList = [{mfi: 'Kiva1'}, {mfi: 'Kiva2'}, {mfi: 'Kiva3'},{mfi: 'Kiva4'},];
+	  var loanList = [{loan: 'Loan1'},{loan: 'Loan2'},{loan: 'Loan3'},{loan: 'Loan4'},];
+   return (
       <div>
         <Navbar inverse fixedTop>
           <Grid>
@@ -20,9 +23,12 @@ class App extends Component {
         </Navbar>
         <Jumbotron className="banner">
           <Grid>
+			<div class = "col-lg-9">
             <h1>Kiva TextField</h1>
             <TextField text = "fname" hint="Enter Some Details" typeVal="Int" limit={100} />
-			<NewLoan />
+			<NewLoan label = "mfi" list = {mfiList} hint = "Select MFI Partner"/>
+			<NewLoan label = "loan" list = {loanList} hint = "Select Loan Type"/>
+			</div>
           </Grid>
         </Jumbotron>
       </div>
