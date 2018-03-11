bca5858e522929b737ae4adc5d514f69aa262ece
jlp-io <jap00031@students.stir.ac.uk>
2018-03-10 19:05:38 -0600

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,8 +3,28 @@ import 'bootstrap/dist/css/bootstrap-theme.css'
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
+import { Provider } from 'react-redux'
+import { Route } from 'react-router'
+import { ConnectedRouter } from 'react-router-redux'
+import configureStore, { history } from './store/configureStore'
+import { App, Colors, Navbar, APRRateDisplay, FormOne } from './components'
 import registerServiceWorker from './registerServiceWorker'
 import './styles/index.css'
-
+ 
 ReactDOM.render(<App />, document.getElementById('root'))
-registerServiceWorker()
+const store = configureStore()
+
+ReactDOM.render(
+  <Provider store={store}>
+    <ConnectedRouter history={history}>
+      <div>
+        <Navbar />
+        <Route exact path="/" component={App} />
+        <Route path="/colors" component={Colors} />
+        <Route path="/output" component={APRRateDisplay} />
+		<Route path="/form1" component={FormOne} />
+      </div>
+    </ConnectedRouter>
+  </Provider>,
+  (document.getElementById('root'): any))
+ registerServiceWorker()
