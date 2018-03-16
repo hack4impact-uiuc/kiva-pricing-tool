901fe662924265ee01a47fa0d57136cf3fc4d46a
jlp-io <jap00031@students.stir.ac.uk>
2018-03-16 00:32:45 -0500

    Update TextField.js

--- frontend/src/components/TextField.js
+++ frontend/src/components/TextField.js
@@ -10,72 +10,96 @@ class TextField extends Component {
       id: this.props.text,
       error_message: '',
       type: this.props.typeVal,
-      textBody: ''
+	  userInput: ''
     }
   }
 
   handleChange(e) {
-    let value = e.target.value
-    if (this.props.typeVal.toLowerCase() == 'int') {
-      let tryInt = parseInt(value)
-      let limit = parseInt(this.props.limit)
-      if (isNaN(tryInt)) {
-        this.setState({ error_message: 'error in input: should be an integer' })
-      } else {
-        if (tryInt > limit) {
-          this.setState({ error_message: 'input limit succeeded' })
-        } else {
-          this.setState({ error_message: '' })
+    var value = e.target.value
+    var input = value.split('')
+    var foundError = false
+
+    if (this.props.typeVal == 'String' || this.props.typeVal == 'string') {
+      if (value.length <= this.props.limit) {
+        for (var i = 0; i < input.length; i++) {
+          if (!isNaN(input[i]) && input[i] != ' ') {
+            foundError = true
+          } else {
+            this.setState({error_message: ''})
+          }
         }
+        if (foundError) {
+          this.setState({ error_message: 'error in input: should be a word' })
+        }
+      } else {
+        this.setState({ error_message: 'error in input' })
       }
     }
 
-    if (this.props.typeVal.toLowerCase() == 'float') {
-      let tryFloat = parseFloat(value)
-      let limit = parseInt(this.props.limit)
-      if (isNaN(tryFloat)) {
-        this.setState({ error_message: 'error in input: should be a decimal' })
-      } else {
-        if (tryFloat > limit) {
-          this.setState({ error_message: 'input limit succeeded' })
-        } else {
-          this.setState({ error_message: '' })
+    if (this.props.typeVal == 'Int' || this.props.typeVal == 'int') {
+      if (value <= this.props.limit) {
+        for (var i = 0; i < input.length; i++) {
+          if (isNaN(input[i])) {
+            foundError = true
+          } else {
+            this.setState({ error_message: '' })
+          }
         }
+        if (foundError) {
+          this.setState({ error_message: 'error in input: should be a number' })
+        }
+      } else {
+        this.setState({ error_message: 'error in input' })
       }
     }
 
-    if (this.props.typeVal.toLowerCase() == 'string') {
-      let tryString = /^[a-zA-Z]+$/.test(value) // damn look at that thicc ass regex
-      if (!tryString) {
-        this.setState({
-          error_message: 'error in input: should only have letters'
-        })
+    if (this.props.typeVal == 'Float' || this.props.typeVal == 'float') {
+      if (value <= this.props.limit) {
+        for (var i = 0; i < input.length; i++) {
+          if (isNaN(input[i]) && input[i] != '.') {
+            foundError = true
+          } else {
+            this.setState({ error_message: '' })
+          }
+        }
+
+        if (value.includes('.') === false) {
+          foundError = true
+        }
+
+        if (foundError) {
+          this.setState({
+            error_message: 'error in input: should be a decimal number'
+          })
+        }
       } else {
-        this.setState({ error_message: '' })
+        this.setState({ error_message: 'error in input' })
       }
     }
-    if (value == '') {
+
+    if (input == '') {
       this.setState({ error_message: '' })
     }
-    this.setState({ textBody: value })
+	
+	if (!foundError) {
+		this.setState({userInput: value})
+	}
   }
 
   render() {
     if (!this.state.valid) {
-      let error = this.state.error_message
+      var error = this.state.error_message
     }
     return (
       <div id="className">
         {this.props.id}:
         <input
-          className="form-control input-sm"
+		  class="form-control input-sm"
           type={this.props.input_type}
           id={this.props.text}
           placeholder={this.props.hint}
           onChange={event => this.handleChange(event)}
-          required
-          autofocus
-        />
+       />
         <br />
         <p>{this.state.error_message}</p>
       </div>
