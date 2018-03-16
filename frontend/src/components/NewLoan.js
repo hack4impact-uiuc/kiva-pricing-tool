50cbb2a3e68638ebce9403ef3bb8831ab1f02a6a
jlp-io <jap00031@students.stir.ac.uk>
2018-03-16 00:32:31 -0500

    Update NewLoan.js

--- frontend/src/components/NewLoan.js
+++ frontend/src/components/NewLoan.js
@@ -8,9 +8,13 @@ import {Typeahead} from 'react-bootstrap-typeahead'
 
 class NewLoan extends Component {
 	constructor(props) {
-		super(props);
-		this.state = {error_message: "", multiple: false, value: ""};
-		var Typeahead = require('react-bootstrap-typeahead').Typeahead;
+		super(props)
+		this.state = {error_message: "", multiple: false, value: ""}
+		var Typeahead = require('react-bootstrap-typeahead').Typeahead
+	}
+	
+	handleChange(e) {
+		this.setState({value: e.toString()})
 	}
 	
 	render() {
@@ -21,7 +25,7 @@ class NewLoan extends Component {
 			multiple=""
 			options={this.props.list}
 			placeholder ={this.props.hint}
-			onChange={(e) => console.log(this.props.list[2])}
+			onChange={event => this.handleChange(event)}
 		/>
 		<br>
 		</br>
