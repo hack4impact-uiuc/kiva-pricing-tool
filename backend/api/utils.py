6b8ef42a94bf877894b0dd8311e9f8bbcfc5edd0
Deanplayerljx <deanplayerljx@gmail.com>
2018-04-03 18:18:37 -0500

    not yet

--- backend/api/utils.py
+++ backend/api/utils.py
@@ -2,6 +2,9 @@ import json
 from flask import jsonify
 import numpy as np
 
+days_dict = {'days':1, 'weeks':7, 'two-weeks':14, '15 days':15, '4 weeks': 28}
+month_num_to_str_dict = {1:'Jan', 2: 'Feb', 3: 'Mar', 4:'Apr', 5:'May', 6:'Jun', 7:'Jul', 8:'Aug', 9:'Sep', 10:'Oct', 11:'Nov', 12:'Dec'}
+
 def create_response(data={}, status=200, message=''):
     """
     Wraps response in a consistent format throughout the API
@@ -199,7 +202,103 @@ def cal_apr_helper(input_json):
         #### above is experimental
         result[-1] += np.sum(security_deposit) + np.sum(security_deposit_interest_paid)
 
+        # build the repayment schedule matrix
+        schedule_matrix = []
+        period_arr = range(num_installment+1)
+        schedule_matrix.append(period_arr)
+        date_arr = []
+        schedule_matrix.append(date_arr)
+        days_arr = []
+        schedule_matrix.append(days_arr)
+        principal_distributed_arr = np.zeros(num_installment+1)
+        principal_distributed_arr[0] = loan_amount
+        schedule_matrix.append(principal_distributed_arr)
+        schedule_matrix.append(principal_paid_arr)
+        schedule_matrix.append(balance_arr)
+        schedule_matrix.append(interest_paid_arr)
+        schedule_matrix.append(fees_paid)
+        schedule_matrix.append(insurance_paid)
+        schedule_matrix.append(taxes)
+        schedule_matrix.append(security_deposit)
+        schedule_matrix.append(interest_paid_arr)
+        schedule_matrix.append(deposit_)
+
         return round_float(np.irr(result) * periods_per_year[installments_period_dict[installment_time_period]] * 100,2)
     except:
         #TODO status code not sure 
         return None
+
+#  helper function for calculate the number of days in the specified period
+def get_num_days(period, prev_date):
+    if period == 'months':
+        return monthrange(prev_date.year, prev_date.month)[1]
+    elif period == 'quarters':
+        days_aggreg = 0
+        for idx in range(3):
+            days_aggreg += monthrange(prev_date.year, prev_date.month)[1]
+            # update the prev_date forward by one month
+            prev_date += datetime.timedelta(days=get_num_days('months', prev_date))
+        return days_aggreg
+    elif period == 'half-years':
+        days_aggreg = 0
+        for idx in range(6):
+            days_aggreg += monthrange(prev_date.year, prev_date.month)[1]
+            # update the prev_date forward by one month
+            prev_date += datetime.timedelta(days=get_num_days('months', prev_date))
+        return days_aggreg
+
+    elif period == 'years':
+        if calendar.isleap(prev_date.year):
+            return 366
+        else:
+            return 365
+    else:
+        return days_dict[period]
+
+# calculate the days column on repayment schedule when directed from input form
+def calc_origin_days(day, month, year, installment_time_period):
+    date_arr = []
+    day_num_arr = []
+    start_date = datetime.datetime(year=start_year, month=start_month, day=start_day)
+    prev_date = start_date
+    day_num_arr.append(0)
+    start_date_str = '{0}-{1}-{2}'.format(start_date.day, month_num_to_str_dict[start_date.month], start_date.year)
+    date_arr.append(start_date_str)
+
+    for idx in range(num_installment):
+        days_to_incre = get_num_days(installment_time_period, prev_date)
+        new_date = prev_date + datetime.timedelta(days=days_to_incre)
+        new_date_str = '{0}-{1}-{2}'.format(new_date.day, month_num_to_str_dict[new_date.month], new_date.year)
+        date_arr.append(new_date_str)
+        day_num_arr.append(days_to_incre)
+        prev_date = new_date
+    return date_arr, day_num_arr
+
+#######
+#For repayment schedule date and days on change
+#######
+# recalculate the days column on repayment schedule 
+def on_change_day(input_date_arr, input_day_arr, change_row_idx, change_val, prev_changes):
+    new_date_arr = []
+    new_day_num_arr = []
+    prev_changes[change_row_idx] = change_val
+    date_col = input_date_arr
+    day_col = input_day_arr
+    print (input_day_arr)
+    start_date = datetime.datetime.strptime(date_col[0], '%d-%b-%Y')
+    prev_date = start_date
+    new_date_arr.append(date_col[0])
+    new_day_num_arr.append(0)
+    for idx in range(1,len(date_col)):
+        if prev_changes[idx] != 0:
+            days_to_incre = prev_changes[idx]
+        else:
+            days_to_incre = get_num_days(installment_time_period, prev_date)
+            
+        new_date = prev_date + datetime.timedelta(days=days_to_incre)
+        new_date_str = '{0}-{1}-{2}'.format(new_date.day, month_num_to_str_dict[new_date.month], new_date.year)
+        new_date_arr.append(new_date_str)
+        new_day_num_arr.append(days_to_incre)
+        prev_date = new_date
+
+    return new_date_arr, new_day_num_arr
