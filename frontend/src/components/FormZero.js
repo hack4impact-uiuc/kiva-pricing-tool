c0cfd9a7917ce2cc4c3eaca95935355dd096ab01
jlp-io <jap00031@students.stir.ac.uk>
2018-04-05 09:38:12 -0500

    Update FormZero.js
    getversionnum complete

--- frontend/src/components/FormZero.js
+++ frontend/src/components/FormZero.js
@@ -5,45 +5,62 @@ import TextField from './TextField'
 import LiveSearch from './LiveSearch'
 import Button from './Button'
 import axios from 'axios'
+import { Typeahead } from 'react-bootstrap-typeahead'
 
 class FormZero extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      partner_names: [],
-      loan_themes: [],
-      selectedPartnerName: '',
-      selectedLoanTheme: '',
-      selectedLoanProduct: '',
-      disableButton: '',
-      errorMessage: ''
+	  partner_names: [],
+   	loan_themes: [],
+	  selectedPartnerName: '',
+    selectedLoanTheme: '',
+    selectedLoanProduct: 'small loan',
+    disableButton: '',
+    errorMessage: '',
+	  versionNum: '',
     }
   }
 
   componentDidMount() {
     axios.get('http://127.0.0.1:3453/partnerThemeLists').then(response => {
+	  console.log(response.data.result)
       this.setState({ partner_names: response.data.result.partners })
       this.setState({ loan_themes: response.data.result.themes })
     })
   }
-
+  
+  handleMFIChange(e) {
+    this.setState({ selectedPartnerName: e })
+  }
+  
+  handleLoanChange(e) {
+    this.setState({ selectedLoanTheme: e })
+  }
+  
+  handleLoanProductChange(e) {
+    this.setState({ selectedLoanProduct: e })
+  }
+  
   render() {
     return (
       <Grid>
         <Form>
           {/* <div class = "col-lg-11"> */}
-          <LiveSearch
-            ref="mfi"
-            label="mfi"
-            list={this.state.partner_names}
-            hint="Select MFI Partner"
-          />
+          <Typeahead
+          labelKey={this.props.label}
+          multiple=""
+          options={this.state.partner_names}
+          placeholder="select MFI partner"
+     	  onChange={this.handleMFIChange.bind(this)}
+		  />
           <br />
-          <LiveSearch
-            ref="loan"
-            label="loan"
-            list={this.state.loan_themes}
-            hint="Select Loan Type"
+		  <Typeahead
+          labelKey={this.props.label}
+          multiple=""
+          options={this.state.loan_themes}
+          placeholder="select loan product"
+     	  onChange={this.handleLoanChange.bind(this)}
           />
           <br />
           <TextField
@@ -53,6 +70,7 @@ class FormZero extends Component {
             hint="i.e. small loan"
             typeVal="String"
             limit={100}
+        	onChange={this.handleLoanProductChange.bind(this)}
           />
           <Button
             disable={this.state.disableButton}
@@ -61,12 +79,28 @@ class FormZero extends Component {
             onClickHandler={e => {
               this.handleClick
             }}
-            formProps={
-              (this.state.selectedPartnerName,
-              this.state.selectedLoanTheme,
-              this.state.selectedLoanProduct)
-            }
           />
+		  <Button
+            disable={this.state.disableButton}
+            name="Back"
+            url=""
+            onClickHandler={e => {
+              this.handleClick
+            }}
+			/>
+
+		  <button
+            type="button"
+            disabled={this.props.disable}
+			onClick={() => {
+				axios.get('http://127.0.0.1:3453/getVersionNum?partner_name=' + this.state.selectedPartnerName + '&theme=' + this.state.selectedLoanTheme + '&product=' + this.state.selectedLoanProduct).then(response => {
+					this.setState({versionNum: response.data.result})
+				}
+				)
+			}
+			}
+			>Find Version</button>
+			<p>{this.state.versionNum}</p>
           {/* </div> */}
         </Form>
       </Grid>
