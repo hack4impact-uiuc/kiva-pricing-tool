2248e634a223f62454dd7eae0533b1c52676ca49
Roy Chiu <rychiu13@gmail.com>
2018-04-17 23:56:04 -0500

    Home page styling

--- frontend/src/components/IntroPage.js
+++ frontend/src/components/IntroPage.js
@@ -9,6 +9,9 @@ import {
   Col
 } from 'react-bootstrap'
 import './../styles/app.css'
+import editimage from './../media/edit.jpg'
+import magnifyingglassimage from './../media/magnifying-glass.jpg'
+import toolsimage from './../media/tools.jpg'
 import Button from './Button'
 
 class IntroPage extends Component {
@@ -41,69 +44,70 @@ class IntroPage extends Component {
         </Row>
 
         <Row>
-          <Col sm={4} md={4}>
-            <p>
-              Lorem ipsum dolor sit amet, id fastidii detraxit sententiae per.
-              Id has deserunt omittantur, sonet legimus civibus an mel. At
-              noluisse reprehendunt his. Lorem convenire vulputate nec ei, nisl
-              principes gubergren pro in. Affert soleat voluptua sea te, ne
-              decore tempor oportere nam. Ad illum ponderum singulis mea. Qui
-              autem fabulas ad, usu porro legere vidisse ea.
+          <Col sm={4} md={4} className="padded-element-vertical">
+            <div className="img-container-center">
+              <img src={editimage} />
+            </div>
+            <PageHeader className="page-header-montserrat padded-element-vertical bs-center">
+              Create a Loan
+            </PageHeader>
+            <p className="padded-element-vertical padded-element-horizontal-lg bs-center">
+              Create a new loan with a combination of inputs - fees, insurance,
+              taxes, interest rate, etc.
             </p>
+            <div className="bs-center padded-element-vertical">
+              <Button
+                name="New Loan"
+                url="newloan"
+                onClickHandler={() => {
+                  console.log('going to new loan')
+                }}
+              />
+            </div>
           </Col>
 
-          <Col sm={4} md={4}>
-            <p>
-              Lorem ipsum dolor sit amet, id fastidii detraxit sententiae per.
-              Id has deserunt omittantur, sonet legimus civibus an mel. At
-              noluisse reprehendunt his. Lorem convenire vulputate nec ei, nisl
-              principes gubergren pro in. Affert soleat voluptua sea te, ne
-              decore tempor oportere nam. Ad illum ponderum singulis mea. Qui
-              autem fabulas ad, usu porro legere vidisse ea.
+          <Col sm={4} md={4} className="padded-element-vertical">
+            <div className="img-container-center">
+              <img src={magnifyingglassimage} />
+            </div>
+            <PageHeader className="page-header-montserrat padded-element-vertical bs-center">
+              Find a Loan
+            </PageHeader>
+            <p className="padded-element-vertical padded-element-horizontal-lg bs-center">
+              Find a previously submitted loan. Either edit the found loan or
+              create a new, duplicated version of the loan.
             </p>
+            <div className="bs-center padded-element-vertical">
+              <Button
+                name="Find Loan"
+                url="findloan"
+                onClickHandler={() => {
+                  console.log('going to find loan')
+                }}
+              />
+            </div>
           </Col>
 
-          <Col sm={4} md={4}>
-            <p>
-              Lorem ipsum dolor sit amet, id fastidii detraxit sententiae per.
-              Id has deserunt omittantur, sonet legimus civibus an mel. At
-              noluisse reprehendunt his. Lorem convenire vulputate nec ei, nisl
-              principes gubergren pro in. Affert soleat voluptua sea te, ne
-              decore tempor oportere nam. Ad illum ponderum singulis mea. Qui
-              autem fabulas ad, usu porro legere vidisse ea.
+          <Col sm={4} md={4} className="padded-element-vertical">
+            <div className="img-container-center">
+              <img src={toolsimage} />
+            </div>
+            <PageHeader className="page-header-montserrat padded-element-vertical bs-center">
+              Admin Tools
+            </PageHeader>
+            <p className="padded-element-vertical padded-element-horizontal-lg bs-center">
+              Add, remove, and edit the accessible Field partners and Loan Theme
+              lists.
             </p>
-          </Col>
-        </Row>
-
-        <Row>
-          <Col sm={4} md={4}>
-            <Button
-              name="New Loan"
-              url="newloan"
-              onClickHandler={() => {
-                console.log('going to new loan')
-              }}
-            />
-          </Col>
-
-          <Col sm={4} md={4}>
-            <Button
-              name="Find Loan"
-              url="findloan"
-              onClickHandler={() => {
-                console.log('going to find loan')
-              }}
-            />
-          </Col>
-
-          <Col sm={4} md={4}>
-            <Button
-              name="Admin Tools"
-              url="adminmain"
-              onClickHandler={() => {
-                console.log('going to admin tools')
-              }}
-            />
+            <div className="bs-center padded-element-vertical">
+              <Button
+                name="Admin Tools"
+                url="adminmain"
+                onClickHandler={() => {
+                  console.log('going to admin tools')
+                }}
+              />
+            </div>
           </Col>
         </Row>
       </Grid>
