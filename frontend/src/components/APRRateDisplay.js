90bb8e19688e0cec18927ec138b5d6da9b8570e1
jlp-io <jap00031@students.stir.ac.uk>
2018-04-27 18:22:43 -0500

    Update APRRateDisplay.js

--- frontend/src/components/APRRateDisplay.js
+++ frontend/src/components/APRRateDisplay.js
@@ -427,13 +427,13 @@ class APRRateDisplay extends Component {
 	<button onClick={this.getCSV.bind(this)}>Download CSV</button>
         <div class = "col-lg-4 pull-right">
 	{this.state.isHidden &&
-	  <ul class = "nav nav-pills nav-stacked">
-	    <li role = "presentation" class={this.state.barclass}><a onClick={()=>this.changeChart("bar")}>Bar</a></li>
-	    <li role = "presentation" class={this.state.lineclass}><a onClick={()=>this.changeChart("line")}>Line</a></li>
-	    <li role = "presentation" class={this.state.areaclass}><a onClick={()=>this.changeChart("area")}>Area</a></li>
-	  </ul>}
-    	</div>
-	<KivaChart visualType={this.state.visualType} data={this.state.data}></KivaChart>}
+	<ul class = "nav nav-pills nav-stacked">
+		<li role = "presentation" class={this.state.barclass}><a onClick={()=>this.changeChart("bar")}>Bar</a></li>
+		<li role = "presentation" class={this.state.lineclass}><a onClick={()=>this.changeChart("line")}>Line</a></li>
+		<li role = "presentation" class={this.state.areaclass}><a onClick={()=>this.changeChart("area")}>Area</a></li>
+	</ul>}
+	</div>
+	{this.state.isHidden && <KivaChart visualType={this.state.visualType} data={this.state.testData}></KivaChart>}
         <br />
         <Button name="Cancel" url="" />
         <Button name="Back" url={formDataReducer.back} />
