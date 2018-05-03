e015feeff09dbe5e4fa7f4ef4ca1806ff620375e
Roy Chiu <rychiu13@gmail.com>
2018-05-02 21:56:27 -0500

    Added check for existing theme and mfi partner after continue on New Loan

--- frontend/src/components/NewLoan.js
+++ frontend/src/components/NewLoan.js
@@ -40,14 +40,34 @@ class NewLoan extends Component {
   inputsEntered() {
     const { formDataReducer } = this.props
     return (
-      !this.isNullOrEmpty(formDataReducer.mfi) &&
-      !this.isNullOrEmpty(formDataReducer.loanType) &&
-      !this.isNullOrEmpty(formDataReducer.productType) &&
+      this.isValidMFI(formDataReducer.mfi) &&
+      this.isValidTheme(formDataReducer.loanType) &&
+      !this.isValidPT(formDataReducer.productType) &&
       !formDataReducer.error // no error
     )
   }
 
-  isNullOrEmpty(input) {
+  isValidMFI(input) {
+    let mfi_exists = false
+    for (let name of this.state.partner_names) {
+      if (name === input) {
+        mfi_exists = true
+      }
+    }
+    return mfi_exists
+  }
+
+  isValidTheme(input) {
+    let theme_exists = false
+    for (let theme of this.state.loan_themes) {
+      if (theme === input) {
+        theme_exists = true
+      }
+    }
+    return theme_exists
+  }
+
+  isValidPT(input) {
     return !input || !input.length
   }
 
