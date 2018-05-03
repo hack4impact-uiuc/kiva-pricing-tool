af611a79f3537e20596a072fa088a15f95c85509
Roy Chiu <rychiu13@gmail.com>
2018-05-02 23:01:39 -0500

    New Loan Title

--- frontend/src/components/APRInputs.js
+++ frontend/src/components/APRInputs.js
@@ -259,6 +259,14 @@ class APRInputs extends Component {
           className="screen-horizontal-centered screen-vertical-centered-grid padded-element-all round-corners-large solid-background"
         >
           <Row>
+            <Col sm={12} md={12} className="bs-center">
+              <PageHeader className="page-header-montserrat page-header-med-font bs-center">
+                {formDataReducer.mfi} | {formDataReducer.loanType} |{' '}
+                {formDataReducer.productType} | {formDataReducer.versionNum}
+              </PageHeader>
+            </Col>
+          </Row>
+          <Row>
             <Row>
               <Col sm={12} md={12}>
                 <h5>* Indicates required field</h5>
