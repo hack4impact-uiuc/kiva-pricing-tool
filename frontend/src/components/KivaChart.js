041f53a1b26bc6a7eaeedcb75d7021f079759071
Megha Mallya <meghamallya1@gmail.com>
2018-05-14 00:02:07 -0500

    added installation doc

--- frontend/src/components/KivaChart.js
+++ frontend/src/components/KivaChart.js
@@ -26,12 +26,6 @@ class KivaChart extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      data3: [
-        { name: 'Group A', value: 400 },
-        { name: 'Group B', value: 300 },
-        { name: 'Group C', value: 300 },
-        { name: 'Group D', value: 200 }
-      ],
       repaymentData: [],
       principalBalance: ''
     }
